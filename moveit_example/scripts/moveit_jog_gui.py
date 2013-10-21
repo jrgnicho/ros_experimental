6996fde29b2670fadb1d6a8be9bb4e6804b1cbb2
jrgnicho@gmail.com <jrgnicho@gmail.com>
2013-10-21 22:47:27 +0000

    added images to buttons

--- moveit_example/scripts/moveit_jog_gui.py
+++ moveit_example/scripts/moveit_jog_gui.py
@@ -3,6 +3,8 @@
 import rospy
 import roslib
 import Tkinter
+import tf2_ros
+import geometry_msgs.msg
 from Tkinter import *
 from Tkinter import Frame
 from Tkinter import Button
@@ -15,16 +17,45 @@ DOWN_ARROW_IMG_PATH = RESOURCES_PATH + "/down_blue.gif"
 LEFT_ARROW_IMG_PATH = RESOURCES_PATH + "/left_blue.gif"
 RIGHT_ARROW_IMG_PATH = RESOURCES_PATH + "/right_blue.gif"
 
+# displacement constants
+DELTA_X = 0.01 #meters
+DELTA_Y = 0.01 #meters
+DELTA_Z = 0.01 #meters
+
+# topics
+TRANSFORM_STAMPED_TOPIC = "tcp_delta_transform"
+
+# frame id
+WORLD_FRAME = "world_frame"
+
 class JogGui(Frame):
     
     def __init__(self,tk_root):
         
+        # tk initialization
         Frame.__init__(self,tk_root)
         #self.frame_ = Frame(tk_root)
         self.tk_root_ = tk_root
         self.pack(side=Tkinter.TOP);
         
+        # images
         self.images_ = []
+        
+        # ros initialization
+        rospy.init_node("moveit_tcp_trasform_publisher")
+        
+        # publisher
+        self.tf_stamped_publisher_ = rospy.Publisher(TRANSFORM_STAMPED_TOPIC,geometry_msgs.msg.TransformStamped)
+        
+        # transform
+        t = geometry_msgs.msg.TransformStamped()
+        t.header.frame_id = WORLD_FRAME
+        t.transform.rotation.x = 0
+        t.transform.rotation.y = 0
+        t.transform.rotation.z = 0
+        t.transform.rotation.w = 1
+        self.transform_msg_ = t
+        
         
     def create_layout(self):
         
@@ -32,32 +63,54 @@ class JogGui(Frame):
         self.x_y_frame_ = Frame(self)
         
         # up button
-        self.images_.append(PhotoImage(file=UP_ARROW_IMG_PATH))
-        #self.y_pos_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[0],width=50,height=50)
-        self.y_pos_button_ = Button(self.x_y_frame_,text='/\\')
+        self.images_.append(self.open_image(UP_ARROW_IMG_PATH, 36, 36))
+        self.y_pos_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[0])
+        #self.y_pos_button_ = Button(self.x_y_frame_,text='/\\')
+        self.y_pos_button_.configure(command = lambda arg = DELTA_Y: self.apply_translation(0, arg, 0))
         self.y_pos_button_.grid(row = 0,column = 1)
         
         # down button
-        self.images_.append(PhotoImage(file=DOWN_ARROW_IMG_PATH))
-        #self.y_neg_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[1],width=50,height=50)
-        self.y_neg_button_ = Button(self.x_y_frame_,text='\\/')
+        self.images_.append(self.open_image(DOWN_ARROW_IMG_PATH,36,36))
+        self.y_neg_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[1])
+        #self.y_neg_button_ = Button(self.x_y_frame_,text='\\/')
+        self.y_neg_button_.configure(command = lambda arg = -DELTA_Y: self.apply_translation(0, arg, 0))
         self.y_neg_button_.grid(row = 2,column = 1)
         
         # left button
-        self.images_.append(PhotoImage(file=LEFT_ARROW_IMG_PATH))
-        #self.x_neg_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[2],width=50,height=50)
-        self.x_neg_button_ = Button(self.x_y_frame_,text='<')
+        self.images_.append(self.open_image(LEFT_ARROW_IMG_PATH,36,36))
+        self.x_neg_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[2])
+        #self.x_neg_button_ = Button(self.x_y_frame_,text='<')
+        self.x_neg_button_.configure(command = lambda arg = -DELTA_X: self.apply_translation(arg, 0 , 0))
         self.x_neg_button_.grid(row = 1 ,column = 0)
         
         # right button
-        self.images_.append(PhotoImage(file=RIGHT_ARROW_IMG_PATH))
-        #self.x_pos_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[2],width=50,height=50)
-        self.x_pos_button_ = Button(self.x_y_frame_,text='>')
+        self.images_.append(self.open_image(RIGHT_ARROW_IMG_PATH,36,36))
+        self.x_pos_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[3])
+        #self.x_pos_button_ = Button(self.x_y_frame_,text='>')
+        self.x_pos_button_.configure(command = lambda arg = DELTA_X: self.apply_translation(arg, 0 , 0))
         self.x_pos_button_.grid(row = 1 ,column = 2)
         
         self.x_y_frame_.pack(side=TOP)
         
         return
+    
+    def apply_translation(self,dx = 0,dy=0,dz = 0):
+        
+        #print "delta = %f, %f, %f" %(dx,dy,dz)
+        #self.transform_msg_ = geometry_msgs.msg.TransformStamped()
+        self.transform_msg_.transform.translation.x = dx
+        self.transform_msg_.transform.translation.y = dy
+        self.transform_msg_.transform.translation.z = dz
+        self.tf_stamped_publisher_.publish(self.transform_msg_)
+        return
+    
+    def open_image(self,file_name,size_x = 10,size_y = 10):
+        
+        im = PhotoImage(file = file_name)
+        #print "before resizing: width %i, height %i" %(im.width(),im.height())
+        im = im.subsample(int(im.width()/size_x), int(im.height()/size_y))
+        #print "after resizing: width %i, height %i" %(im.width(),im.height())
+        return im
         
     def run(self):
         
