c8c4882670936302555791ffa876dcd7ad65da0e
jrgnicho@gmail.com <jrgnicho@gmail.com>
2013-10-22 14:56:25 +0000

    added button on click and on release events

--- moveit_example/scripts/moveit_jog_gui.py
+++ moveit_example/scripts/moveit_jog_gui.py
@@ -54,7 +54,7 @@ class JogGui(Frame):
         t.transform.rotation.y = 0
         t.transform.rotation.z = 0
         t.transform.rotation.w = 1
-        self.transform_msg_ = t
+        self.transform_msg_ = t        
         
         
     def create_layout(self):
@@ -66,33 +66,47 @@ class JogGui(Frame):
         self.images_.append(self.open_image(UP_ARROW_IMG_PATH, 36, 36))
         self.y_pos_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[0])
         #self.y_pos_button_ = Button(self.x_y_frame_,text='/\\')
-        self.y_pos_button_.configure(command = lambda arg = DELTA_Y: self.apply_translation(0, arg, 0))
+        #self.y_pos_button_.configure(command = lambda arg = DELTA_Y: self.apply_translation(0, arg, 0))
+        self.y_pos_button_.bind("<Button-1>",lambda event,arg = DELTA_Y: self.apply_translation(0, arg, 0))
+        self.y_pos_button_.bind("<ButtonRelease-1>", lambda event,arg = 0.0: self.apply_translation(0, arg, 0))
         self.y_pos_button_.grid(row = 0,column = 1)
         
         # down button
         self.images_.append(self.open_image(DOWN_ARROW_IMG_PATH,36,36))
         self.y_neg_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[1])
         #self.y_neg_button_ = Button(self.x_y_frame_,text='\\/')
-        self.y_neg_button_.configure(command = lambda arg = -DELTA_Y: self.apply_translation(0, arg, 0))
+        #self.y_neg_button_.configure(command = lambda arg = -DELTA_Y: self.apply_translation(0, arg, 0))
+        self.y_neg_button_.bind("<Button-1>",lambda event,arg = -DELTA_Y: self.apply_translation(0, arg, 0))
+        self.y_neg_button_.bind("<ButtonRelease-1>", lambda event,arg = 0.0: self.apply_translation(0, arg, 0))
         self.y_neg_button_.grid(row = 2,column = 1)
         
         # left button
         self.images_.append(self.open_image(LEFT_ARROW_IMG_PATH,36,36))
         self.x_neg_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[2])
         #self.x_neg_button_ = Button(self.x_y_frame_,text='<')
-        self.x_neg_button_.configure(command = lambda arg = -DELTA_X: self.apply_translation(arg, 0 , 0))
+        #self.x_neg_button_.configure(command = lambda arg = -DELTA_X: self.apply_translation(arg, 0 , 0))
+        self.x_neg_button_.bind("<Button-1>",lambda event,arg = -DELTA_X: self.apply_translation(arg, 0 , 0))
+        self.x_neg_button_.bind("<ButtonRelease-1>", lambda event,arg = 0.0: self.apply_translation(arg, 0 , 0))
         self.x_neg_button_.grid(row = 1 ,column = 0)
         
         # right button
         self.images_.append(self.open_image(RIGHT_ARROW_IMG_PATH,36,36))
         self.x_pos_button_ = Button(self.x_y_frame_,justify=LEFT,image=self.images_[3])
         #self.x_pos_button_ = Button(self.x_y_frame_,text='>')
-        self.x_pos_button_.configure(command = lambda arg = DELTA_X: self.apply_translation(arg, 0 , 0))
+        #self.x_pos_button_.configure(command = lambda arg = DELTA_X: self.apply_translation(arg, 0 , 0))
+        self.x_pos_button_.bind("<Button-1>",lambda event,arg = DELTA_X: self.apply_translation(arg, 0 , 0))
+        self.x_pos_button_.bind("<ButtonRelease-1>", lambda event,arg = 0.0: self.apply_translation(arg, 0 , 0))
         self.x_pos_button_.grid(row = 1 ,column = 2)
         
         self.x_y_frame_.pack(side=TOP)
         
         return
+    
+    def publish_msg(self):        
+        
+        self.tf_stamped_publisher_.publish(self.transform_msg_)
+        self.after(400, self.publish_msg)
+        
     
     def apply_translation(self,dx = 0,dy=0,dz = 0):
         
@@ -101,7 +115,6 @@ class JogGui(Frame):
         self.transform_msg_.transform.translation.x = dx
         self.transform_msg_.transform.translation.y = dy
         self.transform_msg_.transform.translation.z = dz
-        self.tf_stamped_publisher_.publish(self.transform_msg_)
         return
     
     def open_image(self,file_name,size_x = 10,size_y = 10):
@@ -115,6 +128,7 @@ class JogGui(Frame):
     def run(self):
         
         self.create_layout()
+        self.publish_msg()
         self.tk_root_.mainloop()
         return
         
